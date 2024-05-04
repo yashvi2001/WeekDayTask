cb72c3e1859242dd75698bd40a505f95e95b963e
yashvi2001 <yamyashvi201@gmail.com>
2024-05-04 17:50:02 +0530

    sidebar : initial ui

---
+++ app/components/JobCard.jsx
@@ -0,0 +1,17 @@
+const JobCard =() =>{
+    return (
+        <div className="card-container">
+            <div className="card-title">
+                <img src="/next.svg" height={82} width={82}/>
+                <div>
+                    FlexWash Technologies
+                    <br/>
+                    Senior Engineer
+                    <br/>
+                    India | Exp: 5-5 years
+                </div>
+            </div>
+        </div>
+    )
+}
+export default JobCard
